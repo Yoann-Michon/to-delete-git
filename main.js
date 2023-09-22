7af4ac07f4a429d1a2c2aac5889371b0a43a0dd4
Yoann-Michon <yoann972.wm@outlook.com>
2023-09-22 13:23:00 +0200

    new-feature

--- main.js
+++ main.js
@@ -6,9 +6,15 @@ div.classList.add('container')
 const input = document.createElement('input')
 input.setAttribute('type', 'text')
 
+const button=document.createElement('button')
+button.setAttribute('type', 'button')
+button.style.width = '40px';
+button.style.height = '15px';
 
 div.addEventListener('click',()=>{
+
 })
 
 div.appendChild(input)
+div.appendChild(button)
 app.appendChild(div)
