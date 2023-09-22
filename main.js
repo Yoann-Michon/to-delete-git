24121835633a780fcdc40c41a940566ea3211f9c
Yoann-Michon <yoann972.wm@outlook.com>
2023-09-22 12:20:06 +0200

    new-feature

--- main.js
+++ main.js
@@ -2,18 +2,13 @@ const app = document.querySelector('#app')
 
 const div = document.createElement('div')
 div.classList.add('container')
-div.style.backgroundColor= 'red'
-div.innerText = 'Hello World'
-div.setAttribute('type, text')
 
-div.classList.add('input')
-div.setAttribute('type, radio')
-
-div.classList.add('button')
-div.setAttribute('type, button')
+const input = document.createElement('input')
+input.setAttribute('type', 'text')
 
 
 div.addEventListener('click',()=>{
-  div.style.backgroundColor = 'blue'
 })
+
+div.appendChild(input)
 app.appendChild(div)
